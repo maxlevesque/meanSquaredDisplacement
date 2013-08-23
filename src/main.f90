434495d53ced11713281776b8715a6da490bc022
Maximilien Levesque <maximilien.levesque@gmail.com>
2013-08-23 11:41:00 +0200

    Works with a kind of time-periodic boundary conditions

--- src/main.f90
+++ src/main.f90
@@ -7,41 +7,23 @@ program meanSquaredDisplacement
     implicit none
     character(len("msd.out")) :: outputFile = "msd.out"
     integer :: Nat
-    integer :: i, nbTimeStepsInTraj, iostat, dt, t, nline, nt
+    integer :: i, nbTimeStepsInTraj, iostat, dt, t, nt
     integer, parameter :: x=1, y=2, z=3
     double precision, dimension(:,:,:), allocatable :: r ! position of site i at timestep t
-    double precision :: tmp ! dummy
+    double precision :: tmp, lx, ly, lz
     double precision, dimension(:), allocatable :: msd
     character(len=300) :: arg, trajectoryFileName
-    
-    call get_command_argument(1,arg,status=i)
-    if( i < 0 ) then
-        stop "STOP. The length of the argument is too big for me :( "
-    else if ( i > 0 ) then
-        stop "Argument retrieval failed. You should execute the program with the number of atoms as argument, e.g. in ./msd 10 "
-    end if
-    read(arg,*) Nat
 
-    call get_command_argument(2,arg,status=i)
-    if( i < 0 ) then
-        stop "STOP. The length of the argument is too big for me :( "
-    else if ( i > 0 ) then
-        stop "Argument retrieval failed. You should execute the program with the number of atoms as argument, e.g. in ./msd 10 "
-    end if
-    trajectoryFileName = trim(adjustl(arg))
+    ! read all arguments that MUST be given at execution
+    call readArguments(lx,ly,lz,Nat,trajectoryFileName)
 
-    
-    call opentraj
-    ! computes the number of lines in traj.in and deduces the number of timesteps
-    nline = -1
-    do while (iostat == 0)
-        read(10,*,iostat=iostat) tmp
-        nline = nline + 1
-    end do
-    call closetraj
-    nbTimeStepsInTraj = nline/Nat
+    ! deduce the number of timesteps in the trajectory from the number of lines in the trajectory file
+    nbTimeStepsInTraj = NbOfLinesInTraj()/Nat
 
-    ! allocate consequently
+    print*,'You have' ,nbTimeStepsInTraj,' time steps in your trajectory file ',trim(adjustl(trajectoryFileName))
+    print*,'Please be patient. Everything seems fine... Multiorigin effect! ;)'
+
+    ! allocate consequently what will store the MSD(t)
     allocate( msd(nbTimeStepsInTraj-1), source=0.d0 )
         
     ! read positions of all sites i at all timesteps t
@@ -54,13 +36,47 @@ program meanSquaredDisplacement
     end do
     call closetraj
 
+    ! MSD(t) will be written in file unit 11
     open(11,file=outputfile)
 
-    ! compute msd(dt)= <|r_i(t)-r_i(t+dt)|²>_{i,t}
+    ! compute msd(dt)= <min{|r_i(t)-r_i(t+dt)|}_{PBC(r_i(t+dt))}²>_{i,t}
+    msd=0.d0
     do dt = 1, nbTimeStepsInTraj-1
         nt = nbTimeStepsInTraj-dt
         do t = 1, nt
-            msd(dt) = msd(dt) +sum(  (r(:,t,x)-r(:,t+dt,x))**2 + (r(:,t,y)-r(:,t+dt,y))**2 + (r(:,t,z)-r(:,t+dt,z))**2   )/dble(Nat) ! average over sites i
+            tmp = 0.d0
+            do i = 1, Nat
+                tmp = tmp + min(    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,ly,lz] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,ly,0.d0] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,ly,-lz] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,0.d0,lz] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,0.d0,0.d0] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,0.d0,-lz])   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,-ly,lz] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,-ly,0.d0] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [lx,-ly,-lz])  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,ly,lz] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,ly,0.d0] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,ly,-lz])   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,0.d0,lz] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,0.d0,0.d0] )   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,0.d0,-lz])   ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,-ly,lz] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,-ly,0.d0] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [0.d0,-ly,-lz])  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,ly,lz] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,ly,0.d0] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,ly,-lz])  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,0.d0,lz] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,0.d0,0.d0] )  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,0.d0,-lz])  ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,-ly,lz] ) ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,-ly,0.d0] ) ,&
+                                    norm2( r(i,t,:) - r(i,t+dt,:) - [-lx,-ly,-lz])  &
+                                )**2
+            end do
+            msd(dt) = msd(dt) + tmp/dble(Nat)
+!~             sum(  (r(:,t,x)-r(:,t+dt,x))**2 + (r(:,t,y)-r(:,t+dt,y))**2 + (r(:,t,z)-r(:,t+dt,z))**2   )/dble(Nat) ! average over sites i
         end do
         msd(dt) = msd(dt) /dble(nt) ! average over reference time t
         write(11,*) dt, msd(dt)
@@ -94,5 +110,63 @@ program meanSquaredDisplacement
         end if
     end subroutine
 
+    function NbOfLinesInTraj()
+        integer :: NbOfLinesInTraj
+        call opentraj
+        ! computes the number of lines in traj.in and deduces the number of timesteps
+        NbOfLinesInTraj = -1
+        do while (iostat == 0)
+            read(10,*,iostat=iostat) tmp
+            NbOfLinesInTraj = NbOfLinesInTraj + 1
+        end do
+        call closetraj
+    end function
+    
+    subroutine readArguments(lx,ly,lz,Nat,trajectoryFileName)
+        double precision, intent(out) :: lx, ly, lz
+        integer, intent(out) :: Nat
+        character (len=*), intent(out) :: trajectoryFileName
+        
+        call get_command_argument(1,arg,status=i)
+        if( i < 0 ) then
+            stop "STOP. The length of the argument is too big for me :( "
+        else if ( i > 0 ) then
+            stop "Argument retrieval failed. You should execute the program with the number of atoms as argument, e.g. in ./msd 10 "
+        end if
+        read(arg,*) lx
+    
+        call get_command_argument(2,arg,status=i)
+        if( i < 0 ) then
+            stop "STOP. The length of the argument is too big for me :( "
+        else if ( i > 0 ) then
+            stop "Argument retrieval failed. You should execute the program with the number of atoms as argument, e.g. in ./msd 10 "
+        end if
+        read(arg,*) ly
+    
+        call get_command_argument(3,arg,status=i)
+        if( i < 0 ) then
+            stop "STOP. The length of the argument is too big for me :( "
+        else if ( i > 0 ) then
+            stop "Argument retrieval failed. You should execute the program with the number of atoms as argument, e.g. in ./msd 10 "
+        end if
+        read(arg,*) lz
+    
+        call get_command_argument(4,arg,status=i)
+        if( i < 0 ) then
+            stop "STOP. The length of the argument is too big for me :( "
+        else if ( i > 0 ) then
+            stop "Argument retrieval failed. You should execute the program with the number of atoms as argument, e.g. in ./msd 10 "
+        end if
+        read(arg,*) Nat
+    
+        call get_command_argument(5,arg,status=i)
+        if( i < 0 ) then
+            stop "STOP. The length of the argument is too big for me :( "
+        else if ( i > 0 ) then
+            stop "Argument retrieval failed. You should execute the program with the number of atoms as argument, e.g. in ./msd 10 "
+        end if
+        trajectoryFileName = trim(adjustl(arg))
+    
+    end subroutine
   
 end program
